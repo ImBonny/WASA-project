131201ad760437265d3c939172ed12b6fab00b5e
imbonny <mattei.1910026@studenti.uniroma1.it>
2024-03-07 21:00:43 +0100

    First implement of frontend

--- webui/src/views/HomeView.vue
+++ webui/src/views/HomeView.vue
@@ -1,56 +1,65 @@
 <script>
 export default {
-	data: function() {
+	components: {},
+	data: function () {
 		return {
 			errormsg: null,
-			loading: false,
-			some_data: null,
+			username: "",
+			profile: {
+				id: 0,
+				username: "",
+			},
 		}
 	},
 	methods: {
-		async refresh() {
-			this.loading = true;
-			this.errormsg = null;
-			try {
-				let response = await this.$axios.get("/");
-				this.some_data = response.data;
-			} catch (e) {
-				this.errormsg = e.toString();
+		async doLogin() {
+			if (this.username == "") {
+				this.errormsg = "Username cannot be empty.";
+			} else {
+				try {
+					let response = await this.$axios.post("/session", { username: this.username })
+					this.id = response.data
+					localStorage.setItem("token", this.id);
+					localStorage.setItem("username", this.profile.username);
+					this.$router.push({ path: '/session' })
+				} catch (e) {
+					if (e.response && e.response.status === 400) {
+						this.errormsg = "Form error, please check all fields and try again. If you think that this is an error, write an e-mail to us.";
+						this.detailedmsg = null;
+					} else if (e.response && e.response.status === 500) {
+						this.errormsg = "An internal error occurred. We will be notified. Please try again later.";
+						this.detailedmsg = e.toString();
+					} else {
+						this.errormsg = e.toString();
+						this.detailedmsg = null;
+					}
+				}
 			}
-			this.loading = false;
-		},
+
+		}
 	},
-	mounted() {
-		this.refresh()
-	}
+		mounted() {
+
+		}
 }
 </script>
 
 <template>
-	<div>
-		<div
-			class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-			<h1 class="h2">Home page</h1>
-			<div class="btn-toolbar mb-2 mb-md-0">
-				<div class="btn-group me-2">
-					<button type="button" class="btn btn-sm btn-outline-secondary" @click="refresh">
-						Refresh
-					</button>
-					<button type="button" class="btn btn-sm btn-outline-secondary" @click="exportList">
-						Export
-					</button>
-				</div>
-				<div class="btn-group me-2">
-					<button type="button" class="btn btn-sm btn-outline-primary" @click="newItem">
-						New
-					</button>
-				</div>
-			</div>
-		</div>
-
-		<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
+	<div
+		class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+		<h1 class="h2">Welcome to WASAPhoto</h1>
 	</div>
+	<div class="input-group mb-3">
+		<input type="text" id="username" v-model="username" class="form-control"
+			   placeholder="Insert a username to log in WASAPhoto." aria-label="Recipient's username"
+			   aria-describedby="basic-addon2">
+		<div class="input-group-append">
+			<button class="btn btn-success" type="button" @click="doLogin">Login</button>
+		</div>
+	</div>
+	<ErrorMsg v-if="errormsg" :msg="errormsg"></ErrorMsg>
 </template>
 
 <style>
+
 </style>
