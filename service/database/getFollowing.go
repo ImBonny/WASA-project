2ab2e3f41e78552d1d55e58343940d39cda9eb3e
imbonny <mattei.1910026@studenti.uniroma1.it>
2024-03-17 02:31:45 +0100

    Now the follow is checked when opening a profile

--- service/database/getFollowing.go
+++ service/database/getFollowing.go
@@ -1,26 +1,36 @@
 package database
 
+import (
+	"database/sql"
+	"errors"
+)
+
 func (db *appdbimpl) GetFollowing(id uint64) (*[]Database_user, error) {
 	following := []Database_user{}
-	rows, err := db.c.Query("SELECT * FROM followersDb WHERE userFollowingId = ?", id)
-	if err != nil {
-		return nil, err
+	rows, err := db.c.Query("SELECT userToFollowId FROM followersDb WHERE userFollowingId = ?", id)
+	if errors.Is(err, sql.ErrNoRows) {
+		return &following, nil
 	}
-	err = rows.Close()
 	if err != nil {
 		return nil, err
 	}
 	for rows.Next() {
 		var user Database_user
-		err = rows.Scan(&user.Username, &user.UserId)
-		if err != nil {
+		err1 := rows.Scan(&user.UserId)
+		if err1 != nil {
 			return nil, err
 		}
+		var err2 error
+		user.Username, err2 = db.GetUsernameFromId(user.UserId)
+		if err2 != nil {
+			return nil, err2
+		}
+		print(user.Username, user.UserId)
 		following = append(following, user)
 	}
-	err = rows.Err()
-	if err != nil {
-		return nil, err
+	err3 := rows.Err()
+	if err3 != nil {
+		return nil, err3
 	}
 	return &following, nil
 }
